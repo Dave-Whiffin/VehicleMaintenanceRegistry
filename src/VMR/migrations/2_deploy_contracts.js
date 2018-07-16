f8660bd457be3d45fe21ce13a28f0ba6bceb095e
Dave Whiffin <dave.whiffin@gmail.com>
2018-07-16 17:25:20 +0100

    tidy up

--- src/VMR/migrations/2_deploy_contracts.js
+++ src/VMR/migrations/2_deploy_contracts.js
@@ -1,8 +1,6 @@
 //libs
 var ByteUtilsLib = artifacts.require("ByteUtilsLib.sol");
 var RegistryStorageLib = artifacts.require("RegistryStorageLib.sol");
-var PGL1 = artifacts.require("PGL1");
-var PGC1 = artifacts.require("PGC1");
 
 //contracts
 var EternalStorage = artifacts.require("EternalStorage.sol");
@@ -16,16 +14,10 @@ var MockRegistryLookup = artifacts.require("MockRegistryLookup");
 var MockFeeChecker = artifacts.require("MockFeeChecker.sol");
 
 module.exports = function(deployer) {
-
-  //playground/testing
-  deployer.deploy(PGL1);
-  deployer.link(PGL1, PGC1);
-
   deployer.deploy(ByteUtilsLib);
   deployer.link(ByteUtilsLib, ManufacturerRegistry);
   deployer.deploy(RegistryStorageLib);
   deployer.link(RegistryStorageLib, Registry);
   deployer.link(RegistryStorageLib, ManufacturerRegistry);
   deployer.link(RegistryStorageLib, VehicleRegistry);
-  
 };
