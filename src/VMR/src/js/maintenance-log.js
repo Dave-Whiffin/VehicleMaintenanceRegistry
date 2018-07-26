940dfc123b48f2e66898f3a4631862b82b93b21a
Dave Whiffin <dave.whiffin@gmail.com>
2018-07-26 10:39:55 +0100

    more ui work - add log entry docs

--- src/VMR/src/js/maintenance-log.js
+++ src/VMR/src/js/maintenance-log.js
@@ -19,7 +19,8 @@ MaintenanceLog = {
 
   eventsBound: false,
   logAddress: null,
-  maintenanceLog: null,
+  contract: null,
+  maintainerRegistry: null,
   vin: null,
   contractOwner: 0,
   currentAccount: 0,
@@ -37,27 +38,28 @@ MaintenanceLog = {
     ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods "[\"PUT\", \"POST\", \"GET\"]"
     */
 
-
-    $("#maintenanceLogRow").empty();
-
     MaintenanceLog.logAddress = getParameterByName("address");
 
     console.log("maintenance log address: " + MaintenanceLog.logAddress);
 
     ContractFactory.init(async function() {
+
+      console.log("length ipfs:" + lengthInUtf8Bytes("Qmcq1AVPSg2rP7JKV6SHKwTwWZAvhMZTYBAVX7sJJozdMV"));
+
       MaintenanceLog.currentAccount = web3.eth.accounts[0];
-      MaintenanceLog.maintenanceLog = ContractFactory.getMaintenanceLogContract(MaintenanceLog.logAddress);      
-      MaintenanceLog.vin = web3.toUtf8(await MaintenanceLog.maintenanceLog.vin.call());
-      MaintenanceLog.contractOwner = await MaintenanceLog.maintenanceLog.owner.call();
+      MaintenanceLog.contract = ContractFactory.getMaintenanceLogContract(MaintenanceLog.logAddress);      
+      let maintainerRegistryAddress = await MaintenanceLog.contract.maintainerRegistryAddress.call();
+      MaintenanceLog.maintainerRegistry = ContractFactory.getMaintainerRegistryContract(maintainerRegistryAddress);
+      MaintenanceLog.vin = web3.toUtf8(await MaintenanceLog.contract.vin.call());
+      MaintenanceLog.contractOwner = await MaintenanceLog.contract.owner.call();
       MaintenanceLog.currentUserIsContractOwner = MaintenanceLog.currentAccount == MaintenanceLog.contractOwner;
       MaintenanceLog.bindEvents();
-      return MaintenanceLog.viewMaintenanceLog();
+      return MaintenanceLog.getAndBindEntries();
     });
   },
 
-  getMaintainerRegistry: async function() {
-    let maintainerRegistryAddress = await MaintenanceLog.maintenanceLog.maintainerRegistryAddress.call();
-    return ContractFactory.getMaintainerRegistryContract(maintainerRegistryAddress);
+  getMaintainerRegistry: function() {
+    return MaintenanceLog.maintainerRegistry;
   },
 
   addLogEntry: async function(event) {
@@ -84,14 +86,14 @@ MaintenanceLog = {
     id = web3.fromUtf8(id);
     maintainerId = web3.fromUtf8(maintainerId);
 
-    let isAuthorisedMaintainer = await MaintenanceLog.maintenanceLog.isAuthorised(maintainerId);
+    let isAuthorisedMaintainer = await MaintenanceLog.contract.isAuthorised(maintainerId);
     if(!isAuthorisedMaintainer){
       alert(maintainerId +  " is not an authorised maintainer");
       return;
     }
 
     try{
-       await MaintenanceLog.maintenanceLog.getLogNumber(id);
+       await MaintenanceLog.contract.getLogNumber(id);
        alert("id already exists - it must be unique"); 
        return;
     }
@@ -100,15 +102,13 @@ MaintenanceLog = {
       //the contract throws when the id does not exist
     }
 
-    let maintainerRegistry = await MaintenanceLog.getMaintainerRegistry();
-
-    let isRegisteredAndEnabled = await maintainerRegistry.isMemberRegisteredAndEnabled(maintainerId);
+    let isRegisteredAndEnabled = await MaintenanceLog.maintainerRegistry.isMemberRegisteredAndEnabled(maintainerId);
     if(!isRegisteredAndEnabled) {
       alert(maintainerId + " is not registered or not enabled in the maintainer regsistry");
       return;
     }
 
-    let maintainerOwner = await maintainerRegistry.getMemberOwner(maintainerId);
+    let maintainerOwner = await MaintenanceLog.maintainerRegistry.getMemberOwner(maintainerId);
     if(MaintenanceLog.currentAccount != maintainerOwner) {
       alert("only the owner of the maintainer is allowed to log an entry");
       return;
@@ -116,7 +116,7 @@ MaintenanceLog = {
 
     let date = Math.round(new Date().getTime() / 1000);
 
-    let tx = await MaintenanceLog.maintenanceLog.add(id, maintainerId, date, title, description);
+    let tx = await MaintenanceLog.contract.add(id, maintainerId, date, title, description);
     
     button.html("log entry submitted");
 
@@ -131,9 +131,9 @@ MaintenanceLog = {
       else{
         console.log("transaction finished. status: " + result.status);
 
-        var logNumber = await MaintenanceLog.maintenanceLog.getLogNumber(id);
+        var logNumber = await MaintenanceLog.contract.getLogNumber(id);
         var log = await MaintenanceLog.getWrappedLog(logNumber);
-        MaintenanceLog.viewLogEntry(log);
+        MaintenanceLog.bindEntry(log);
       }
     });
 
@@ -146,48 +146,120 @@ MaintenanceLog = {
     if(!MaintenanceLog.eventsBound){
       $(document).on('click', '.btn-verify-maintenance-log', MaintenanceLog.verify);
       $(document).on('click', '#btn-add-log-entry', MaintenanceLog.addLogEntry);
+      $(document).on('click', '#btn-add-log-doc', MaintenanceLog.addDoc);      
       $(document).on('click', '.add-doc-button', MaintenanceLog.navigateToAddDoc);
-      $(document).on('click', '#btn-add-log-doc', MaintenanceLog.addDoc);
       MaintenanceLog.eventsBound = true;
     }
+  },
+
+  isValidIpfsAddress: function(ipfsAddress) {
+    return lengthInUtf8Bytes(ipfsAddress) === 46;
+  },
+
+  validateAddDoc: async function(logNumber, title, ipfsAddress, callBack) {
+    if(isNaN(logNumber)) {
+      callBack("invalid log number");
+      return false;
+    }
+
+    logNumber = parseInt(logNumber);
+
+    if(logNumber == 0) {
+      callBack("invalid log number");
+      return false;
+    }
+
+    if(title === "") {
+      callBack("invalid title");
+      return false;
+    }
+
+    if(!MaintenanceLog.isValidIpfsAddress(ipfsAddress)) {
+      callBack("invalid ipfs address");
+      return false;
+    }
+
+    let log;
+
+    try{
+      log = await MaintenanceLog.getWrappedLog(logNumber);
+    }
+    catch(err){
+      console.log(err);
+      callBack("not a valid log number");
+      return false;
+    }
+
+    if(log.verified) {
+      callBack("Denied - docs can not be added to verified log entries");
+      return false;
+    }
+
+    let isMaintainerAuthorised = await MaintenanceLog.contract.isAuthorised(log.maintainerId);
+    if(!isMaintainerAuthorised)
+    {
+      callBack("The maintainer is not (or no longer) authorised.");
+      return false;
+    }
+
+    let maintainerOwner = await MaintenanceLog.maintainerRegistry.getMemberOwner(log.maintainerId);
+    
+    if(MaintenanceLog.currentAccount != maintainerOwner) {
+      callBack("Denied - You are not the owner of the maintainer");
+      return false;
+    }
+
+    callBack(null, log);
+    return true;
   },
 
   addDoc: async function(event) {
     event.preventDefault();
 
+    let button = $(event.target);
+    let buttonText = button.html();
+    button.attr("disabled", "disabled");
+    button.html("validating");
+
     let logNumber = $("#new-log-doc-log-number").val();
     let title = $("#new-log-doc-title").val();
     let ipfsAddress = $("#new-log-doc-ipfs-address").val();
 
-    if(isNaN(logNumber)) {
+    let log;
+
+    let valid = await MaintenanceLog.validateAddDoc(logNumber, title, ipfsAddress, function(error, validatedLog) {
+      if(error != null) {
+        console.log(error);
+        alert(error);
+        button.html(buttonText);
+        button.removeAttr("disabled");
+      } else {
+        log = validatedLog;
+      }
+    });
+
+    if(!valid) {
       return;
     }
-
-    logNumber = parseInt(logNumber);
-
-    let log = await MaintenanceLog.getWrappedLog(logNumber);
-
-    if(log.verified) {
-      alert("Denied - docs can not be added to verified log entries");
-      return;
-    }
-
-    let isMaintainerAuthorised = await MaintenanceLog.maintenanceLog.isAuthorised(log.maintainerId);
-    if(!isMaintainerAuthorised)
-    {
-      alert("The maintainer is not (or no longer) authorised.");
-      return;
-    }
-
-    let maintainerRegistry = await MaintenanceLog.getMaintainerRegistry();
-    let maintainerOwner = await maintainerRegistry.getMemberOwner(log.maintainerId);
     
-    if(MaintenanceLog.currentAccount != maintainerOwner) {
-      alert("Denied - You are not the owner of the maintainer");
-      return;
-    }
+    button.html("submitting");
+    let txHash = await MaintenanceLog.contract.addDoc(log.logNumber, title, ipfsAddress);
+    button.removeAttr("disabled");
+    button.html("submitted");
 
-    alert("success - so far");
+    setTimeout(function() {
+      button.html(buttonText);
+    }, 1000);
+
+    await web3.eth.getTransactionReceipt(txHash.tx, async function(error, result) {
+      if(error != null) {
+        console.log(error);
+      }
+      else {
+        console.log(result);
+        await MaintenanceLog.getAndBindDocs(logNumber);
+      }
+    });
 
   },
 
@@ -202,7 +274,7 @@ MaintenanceLog = {
     event.preventDefault();
     let logNumber = parseInt($(event.target).data('id'));
 
-    let receipt = await MaintenanceLog.maintenanceLog.verify(logNumber);
+    let receipt = await MaintenanceLog.contract.verify(logNumber);
     
     let maintenanceLogRow = $('#maintenanceLogRow');
     let panel = maintenanceLogRow.find(".maintenance-log-entry-panel[data-id=" + logNumber + "]");
@@ -232,23 +304,25 @@ MaintenanceLog = {
   },
 
   getWrappedLog: async function(logNumber) {
-    return MaintenanceLog.wrapLog(await MaintenanceLog.maintenanceLog.getLog(logNumber));
+    return MaintenanceLog.wrapLog(await MaintenanceLog.contract.getLog(logNumber));
   },
 
-  viewDocs: async function(logNumber) {
+  getAndBindDocs: async function(logNumber) {
 
     var maintenanceLogRow = $('#maintenanceLogRow');
     var logDocTemplate  = $('#maintenanceLogDocTemplate');
     var docContainer = maintenanceLogRow.find('.log-doc-panel[data-id=' + logNumber + ']');
 
+    docContainer.empty();
+
     console.log("retrieving doc count for log #" + logNumber);
 
-    var docCount = await MaintenanceLog.maintenanceLog.getDocCount(logNumber);
+    var docCount = await MaintenanceLog.contract.getDocCount(logNumber);
 
     console.log("Log#:" + logNumber + " DocCount:" + docCount);
 
     for(var i = 1; i <= docCount; i++) {
-        var doc = await MaintenanceLog.maintenanceLog.getDoc(logNumber, i);
+        var doc = await MaintenanceLog.contract.getDoc(logNumber, i);
 
         logDocTemplate.find(".log-doc-number").text(doc[0]);
         logDocTemplate.find(".log-doc-title").text(doc[1]);
@@ -275,7 +349,7 @@ MaintenanceLog = {
     }
   },
 
-  viewLogEntry: function(log) {
+  bindEntry: function(log) {
     var maintenanceLogRow = $('#maintenanceLogRow');
     var maintenanceLogTemplate = $('#maintenanceLogEntryTemplate');
 
@@ -303,20 +377,22 @@ MaintenanceLog = {
 
     maintenanceLogRow.append(maintenanceLogTemplate.html());
 
-    MaintenanceLog.viewDocs(log.logNumber);
+    MaintenanceLog.getAndBindDocs(log.logNumber);
   },
 
-  viewMaintenanceLog: async function() {
+  getAndBindEntries: async function() {
 
+    $("#maintenanceLogRow").empty();
     $(".maintenance-log-vin").text(MaintenanceLog.vin);
     $(".maintenance-log-address").text(MaintenanceLog.logAddress);
     $(".maintenance-log-owner").text(MaintenanceLog.contractOwner);
 
-    var totalCount = await MaintenanceLog.maintenanceLog.getLogCount();
+    var totalCount = await MaintenanceLog.contract.getLogCount();
+    $(".maintenance-log-entry-count").text(totalCount);
 
     for(var i = 1; i <= totalCount; i++) {
       let log = await MaintenanceLog.getWrappedLog(i);
-      MaintenanceLog.viewLogEntry(log);
+      MaintenanceLog.bindEntry(log);
     }
   }
 };
